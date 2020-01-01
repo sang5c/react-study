cba59f09ac81b9611b2f3dabc910e6ded8aa1fc4
sang5c <skah321@gmail.com>
2020-01-01 23:45:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        count: 0,
+        res: {},
+        result: 'test!',
+      result2: '',
+    };
+
+    callApi = () => {
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+          .then(response => response.json())
+          // .then(json => console.log(json))
+          .then(json => this.setState({
+            res: json,
+            result: json.title,
+            result2: json.id
+          }));
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h3>API CALL TEST</h3>
+                <div>
+                  {this.state.result}<br/>
+                  {this.state.result2}<br/>
+                  <button onClick={this.callApi}>test api</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
